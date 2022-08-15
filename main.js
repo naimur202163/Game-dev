d1ed034ac0836e426fea82e82e5783939c61f346
naimur <naimur202163@gmail.com>
2022-08-15 09:29:21 +0600

    scrolled added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const nav = document.querySelector("nav");
+const mobileNav=document.querySelector("nav.mobile-nav");
+
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 60) {
     nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled")
   }
 });
 
